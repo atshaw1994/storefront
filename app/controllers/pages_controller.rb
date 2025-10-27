3cd049c0f7f393202fe3561a227fe819557acc0a
atshaw1994 <atshaw1994@gmail.com>
2025-10-27 15:17:24 -0400

    Header implementation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,22 @@ class PagesController < ApplicationController
   end
 
   def show
+    render params[:id]
+  end
+
+  def home
+    render :home
+  end
+
+  def cart
+    render :cart
+  end
+
+  def contact
+    render :contact
+  end
+
+  def about
+    render :about
   end
 end
